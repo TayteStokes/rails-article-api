6165a31ee041484e74b89da4b151e33f7fff814c
Tayte Stokes <taytestokes9328@gmail.com>
2019-06-12 19:47:03 -0600

    finished full crud for articles controller

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,9 +1,46 @@
 module Api
-    module v1
+    module V1
         class ArticlesController < ApplicationController
             def index
                 articles = Article.order('created_at DESC')
-                render json: {status: 'SUCCESS', message: 'Loaded Articles', data:articles}, status:ok
+                render json: {status: 'SUCCESS', message: 'Loaded Articles', data:articles}, status: :ok
+            end
+
+            def create
+                article = Article.new(article_params)
+
+                if article.save
+                    render json: {status: 'SUCCESS', message: 'Save Article', data:article}, status: :ok
+                else
+                    render json: {status: 'ERROR', message: 'Something Went Wrong, Please Try Again...', data:article.errors}, status: :unprocessable_entity
+                end
+            end
+
+            def show
+                # Use show to return single items
+                article = Article.find(params[:id])
+                render json: {status: 'SUCCESS', message: 'Loaded Article', data:article}, status: :ok
+            end
+
+            def update
+                article = Article.find(params[:id])
+                if article.update_attributes(article_params)
+                    render json: {status: 'SUCCESS', message: 'Article Updated', data:article}, status: :ok
+                else
+                    render json: {status: 'ERROR', message: 'Something Went Wrong, Please Try Again...', data:article.errors}, status: :unprocessable_entity
+                end
+            end
+
+            def destroy
+                article = Article.find(params[:id])
+                article.destroy()
+                render json: {status: 'SUCCESS', message: 'Article Destroyed', data:article}, status: :ok
+            end
+
+            private 
+            # White List article_params
+            def article_params
+                params.permit(:title, :body)
             end
         end
     end
